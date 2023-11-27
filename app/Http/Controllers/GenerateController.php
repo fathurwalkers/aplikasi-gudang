cce5c6714f2295d2fe2250f8b32b3f237832160f
Fathur Walkers <fathurwalkers44@gmail.com>
2023-11-27 05:47:02 -0800

    update

---
+++ app/Http/Controllers/GenerateController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Faker\Factory as Faker;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\{
+    Str,
+    Arr
+};
+use App\Models\{
+    Login,
+};
+
+class GenerateController extends Controller
+{
+    public function generate_user()
+    {
+        //
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,7 @@ use App\Http\Controllers\BackController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect()->route('dashboard');
 });
 
 Route::group(['prefix' => '/dashboard'], function () {
