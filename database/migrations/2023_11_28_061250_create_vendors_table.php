cefdfb59fc75e511ddf90efaf96776b4fe4b2727
Fathur Walkers <fathurwalkers44@gmail.com>
2023-11-27 22:15:08 -0800

    update

--- database/migrations/2023_11_28_061250_create_vendors_table.php
+++ database/migrations/2023_11_28_061250_create_vendors_table.php
@@ -6,26 +6,16 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     *
-     * @return void
-     */
     public function up()
     {
-        Schema::create('vendors', function (Blueprint $table) {
+        Schema::create('vendor', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
         });
     }
 
-    /**
-     * Reverse the migrations.
-     *
-     * @return void
-     */
     public function down()
     {
-        Schema::dropIfExists('vendors');
+        Schema::dropIfExists('vendor');
     }
 };
