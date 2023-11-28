d345cfad3f4a400d3a0e68a802da0fc59c8445c1
Fathur Walkers <fathurwalkers44@gmail.com>
2023-11-27 22:15:28 -0800

    update

--- database/migrations/2023_11_28_061205_create_pegawais_table.php
+++ database/migrations/2023_11_28_061205_create_pegawais_table.php
@@ -6,26 +6,16 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     *
-     * @return void
-     */
     public function up()
     {
-        Schema::create('pegawais', function (Blueprint $table) {
+        Schema::create('pegawai', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
         });
     }
 
-    /**
-     * Reverse the migrations.
-     *
-     * @return void
-     */
     public function down()
     {
-        Schema::dropIfExists('pegawais');
+        Schema::dropIfExists('pegawai');
     }
 };
