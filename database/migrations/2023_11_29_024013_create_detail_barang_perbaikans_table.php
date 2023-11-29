db782c60c68264475aade642e85cb167bf3a0c1c
Fathur Walkers <fathurwalkers44@gmail.com>
2023-11-28 19:12:22 -0800

    update

--- database/migrations/2023_11_29_024013_create_detail_barang_perbaikans_table.php
+++ database/migrations/2023_11_29_024013_create_detail_barang_perbaikans_table.php
@@ -11,12 +11,12 @@ return new class extends Migration
         Schema::create('detail_barang_perbaikan', function (Blueprint $table) {
             $table->id();
 
-            $table->string('detail_perbaikan_nama')->nullable();
-            $table->dateTime('detail_perbaikan_tanggal')->nullable();
-            $table->string('detail_perbaikan_no_invoice')->nullable();
-            $table->string('detail_perbaikan_no_po')->nullable();
+            $table->string('detail_perbaikan_nama_barang')->nullable();
             $table->string('detail_perbaikan_jumlah')->nullable();
-            $table->string('detail_perbaikan_harga_jual_perbarang')->nullable();
+            $table->dateTime('detail_perbaikan_tanggal')->nullable();
+            $table->string('detail_perbaikan_sisa')->nullable();
+            $table->string('detail_perbaikan_gambar')->nullable();
+            $table->text('detail_perbaikan_keterangan')->nullable();
 
             $table->unsignedBigInteger('detail_barang_pembelian_id')->nullable()->default(null);
             $table->foreign('detail_barang_pembelian_id')->references('id')->on('detail_barang_pembelian')->onDelete('cascade');
