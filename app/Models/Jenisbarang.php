cab6bd8093f6e85323b3a897cc02bf6b7cbf7ab4
Fathur Walkers <fathurwalkers44@gmail.com>
2023-11-28 18:08:16 -0800

    update

--- app/Models/Jenisbarang.php
+++ app/Models/Jenisbarang.php
@@ -4,8 +4,17 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\Barang;
 
 class Jenisbarang extends Model
 {
     use HasFactory;
+    protected $table = "jenis_barang";
+    protected $guarded = [];
+    protected $primaryKey = "id";
+
+    public function barang()
+    {
+        return $this->hasMany(Barang::class);
+    }
 }
--- app/Models/Kategoribarang.php
+++ app/Models/Kategoribarang.php
@@ -4,8 +4,17 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\Barang;
 
 class Kategoribarang extends Model
 {
     use HasFactory;
+    protected $table = "kategori_barang";
+    protected $guarded = [];
+    protected $primaryKey = "id";
+
+    public function barang()
+    {
+        return $this->hasMany(Barang::class);
+    }
 }
